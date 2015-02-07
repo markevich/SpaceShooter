9effb6ecab71bc6f20dde0ddcf009c7735e807d4
Slava Markevich <slava.markevich@gmail.com>
2015-02-07 14:11:23 +0300

    Add VFX to asteroids. Add collider logic too.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,9 +2,17 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other){
 		if(other.tag == "Boundary")
 			return;
+
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if(other.tag == "Player")
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 
 		Destroy(other.gameObject);
 		Destroy(gameObject);
