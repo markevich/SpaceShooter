60f990d66042eb69ffe0e22d63f4ed3569243355
Slava Markevich <slava.markevich@gmail.com>
2015-02-07 21:24:40 +0300

    Implement spawn of asteroids waves

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+	public float lifeTime;
+
+	void Start(){
+		Destroy(gameObject, lifeTime);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,14 +4,24 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount = 1;
+	public float spawnWait, startWait, waveWait;
 
 	public void Start(){
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 
-	private void SpawnWaves(){
-		var spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+	private IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds(startWait);
+		while(true){
+			for(int i = 0; i < hazardCount; i++){
+				var spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 
-		Instantiate(hazard, spawnPosition, Quaternion.identity);
+				Instantiate(hazard, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
