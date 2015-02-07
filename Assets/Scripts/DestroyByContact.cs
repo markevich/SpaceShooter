7e63e41ad2dee303a4eb3fe7a8e107246a46d317
Slava Markevich <slava.markevich@gmail.com>
2015-02-07 14:03:39 +0300

    Add asteroids.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Boundary")
+			return;
+
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
