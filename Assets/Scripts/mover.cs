64ee56f1900c088a7ddc8434e85e9c4747ee8cb1
markevich <slava.markevich@gmail.com>
2015-02-07 13:12:03 +0300

    create bolt prefab

---
+++ Assets/Scripts/mover.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour {
+	public float speed = 1;
+
+	void Start(){
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
