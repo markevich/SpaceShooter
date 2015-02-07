1dd81d2348b8d46e73a09a92d738f71255a73fe9
Slava Markevich <slava.markevich@gmail.com>
2015-02-07 21:03:22 +0300

    Add game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	public void Start(){
+		SpawnWaves();
+	}
+
+	private void SpawnWaves(){
+		var spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+		Instantiate(hazard, spawnPosition, Quaternion.identity);
+	}
+}
