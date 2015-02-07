daec2c76a8783d8d48901d25439a040a2b337205
markevich <slava.markevich@gmail.com>
2015-02-07 12:56:22 +0300

    Initial.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1;
+	public float _tilt = 1;
+	
+	public Boundary boundary;
+
+	private Vector3 _velocityVector, _clampedPositionVector = Vector3.zero;
+
+	public void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		_velocityVector.x = moveHorizontal;
+		_velocityVector.z = moveVertical;
+
+		rigidbody.velocity = _velocityVector * speed;
+
+		_clampedPositionVector.x = Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax);
+		_clampedPositionVector.z = Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax);
+
+		rigidbody.position = _clampedPositionVector;
+		rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -_tilt);
+	}
+}
